5de2cfea9104f9605f6e16fc40f9a45a6285926e
jeganvicent <jvincent@boodskap.io>
2024-05-16 17:42:58 +0530

    basic project setup

---
+++ lib/configs.dart
@@ -0,0 +1,7 @@
+class FontConfig {
+  final double? fontSize;
+  final int? fontColor;
+  final bool? fontBold;
+  const FontConfig(
+      {this.fontSize = 14, this.fontColor = 0xFF000000, this.fontBold = false});
+}
---
+++ lib/twinned_session.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_session_manager/flutter_session_manager.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:twinned_api/api/twinned.swagger.dart' as digital;
+
+class TwinnedSession {
+  TwinnedSession._privateConstructor() {}
+
+  Future load({bool debug = true, String host = 'twinned.digital'}) async {
+    if (_inited) return;
+
+    await dotenv.load(fileName: 'settings.txt');
+
+    var session = SessionManager();
+
+    _debug = debug;
+    _host = host;
+
+    _twinned =
+        digital.Twinned.create(baseUrl: Uri.https(_host, '/rest/nocode'));
+    _inited = true;
+
+    await session.update();
+  }
+
+  Future cleanup() async {
+    return SessionManager().destroy();
+  }
+
+  static final TwinnedSession _instance = TwinnedSession._privateConstructor();
+
+  String _authToken = '';
+  bool _debug = true;
+  String _host = '';
+  digital.Twinned _twinned = digital.Twinned.create(
+      baseUrl: Uri.https('twinned.boodskap.io', '/rest/nocode'));
+  bool _inited = false;
+
+  static TwinnedSession get instance => _instance;
+
+  String get authToken => _authToken;
+  set setAuthToken(String authToken) {
+    _authToken = authToken;
+  }
+
+  String get host => _host;
+  bool get debug => _debug;
+  digital.Twinned get twin => _twinned;
+  bool get inited => _inited;
+}
