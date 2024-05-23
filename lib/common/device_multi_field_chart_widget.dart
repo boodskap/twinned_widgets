f5ca7c57b440b671b85b6b221f59c669a4a19fba
Thirumalvalavan <thirumalvalavan99@gmail.com>
2024-05-23 10:20:42 +0530

    added multi field cartesian widget

---
+++ lib/common/device_multi_field_chart_widget.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:nocode_commons/core/base_state.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:twinned_api/twinned_api.dart';
+import 'package:twinned_models/models.dart';
+import 'package:twinned_widgets/palette_category.dart';
+import 'package:twinned_widgets/twinned_session.dart';
+import 'package:twinned_widgets/twinned_widget_builder.dart';
+
+class DeviceMultiFieldChartWidget extends StatefulWidget {
+  final DeviceMultiFieldChartWidgetConfig config;
+  const DeviceMultiFieldChartWidget({super.key, required this.config});
+
+  @override
+  State<DeviceMultiFieldChartWidget> createState() =>
+      _DeviceMultiFieldChartWidgetState();
+}
+
+class _DeviceMultiFieldChartWidgetState
+    extends BaseState<DeviceMultiFieldChartWidget> {
+  final List<SeriesData> _chatSeries = [];
+  bool isValidConfig = false;
+
+  @override
+  void initState() {
+    isValidConfig = widget.config.field.isNotEmpty;
+    isValidConfig = isValidConfig && widget.config.deviceId.isNotEmpty;
+
+    super.initState();
+  }
+
+  @override
+Widget build(BuildContext context) {
+  if (!isValidConfig) {
+    return const Wrap(
+      spacing: 8.0,
+      children: [
+        Text(
+          'Not configured properly',
+          style: TextStyle(color: Colors.red, overflow: TextOverflow.ellipsis),
+        ),
+      ],
+    );
+  }
+
+  return Center(
+    child: SfCartesianChart(
+      title: ChartTitle(text: widget.config.title),
+      legend: const Legend(isVisible: true),
+      series: widget.config.field.map((field) {
+        return LineSeries<SeriesData, num>(
+          name: field,
+          enableTooltip: true,
+          dataSource: _chatSeries,
+          xValueMapper: (SeriesData data, _) => data.stamp,
+          yValueMapper: (SeriesData data, _) => data.values[field],
+          width: 2,
+          markerSettings: const MarkerSettings(
+            isVisible: true,
+            height: 4,
+            width: 4,
+            shape: DataMarkerType.circle,
+            borderWidth: 3,
+            borderColor: Colors.red,
+          ),
+          dataLabelSettings: const DataLabelSettings(
+            isVisible: true,
+            labelAlignment: ChartDataLabelAlignment.auto,
+          ),
+        );
+      }).toList(),
+    ),
+  );
+}
+
+Future load({String? filter, String search = '*'}) async {
+  if (!isValidConfig) return;
+
+  if (loading) return;
+  loading = true;
+
+  _chatSeries.clear();
+
+  await execute(() async {
+    var qRes = await TwinnedSession.instance.twin.queryDeviceHistoryData(
+      apikey: TwinnedSession.instance.authToken,
+      body: EqlSearch(
+        page: 0,
+        size: 100,
+        conditions: [],
+        boolConditions: [],
+        queryConditions: [],
+        mustConditions: [
+          {"exists": {"field": "data.volume"}},
+          {"exists": {"field": "data.temperature_value"}},
+          {"match_phrase": {"deviceId": widget.config.deviceId}},
+        ],
+        sort: {'updatedStamp': 'desc'},
+      ),
+    );
+
+    if (validateResponse(qRes)) {
+      Map<String, dynamic> json = qRes.body!.result! as Map<String, dynamic>;
+      List<dynamic> values = json['hits']['hits'];
+      for (Map<String, dynamic> obj in values) {
+        int millis = obj['p_source']['updatedStamp'];
+        Map<String, dynamic> dataValues = {};
+        for (String field in widget.config.field) {
+          dataValues[field] = obj['p_source']['data'][field];
+        }
+        _chatSeries.add(SeriesData(stamp: millis, values: dataValues));
+      }
+    }
+  });
+
+  loading = false;
+  refresh();
+}
+
+  @override
+  void setup() {
+    load();
+  }
+}
+
+class SeriesData {
+  final num stamp;
+  final Map<String, dynamic> values;
+
+  SeriesData({required this.stamp, required this.values});
+}
+
+
+class DeviceMultiFieldChartWidgetBuilder extends TwinnedWidgetBuilder {
+  @override
+  Widget build(Map<String, dynamic> config) {
+    return DeviceMultiFieldChartWidget(
+        config: DeviceMultiFieldChartWidgetConfig.fromJson(config));
+  }
+
+  @override
+  PaletteCategory getPaletteCategory() {
+    return PaletteCategory.chartsAndGraphs;
+  }
+
+  @override
+  Widget getPaletteIcon() {
+    return const Icon(Icons.stacked_line_chart);
+  }
+
+  @override
+  String getPaletteName() {
+    return "Line Chart";
+  }
+
+  @override
+  BaseConfig getDefaultConfig() {
+    return DeviceMultiFieldChartWidgetConfig();
+  }
+}
--- lib/twinned_widgets.dart
+++ lib/twinned_widgets.dart
@@ -3,6 +3,7 @@ library twinned_widgets;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:twinned_widgets/common/device_cartesian_chart_widget.dart';
+import 'package:twinned_widgets/common/device_multi_field_chart_widget.dart';
 import 'package:twinned_widgets/common/total_and_reporting_asset_widget.dart';
 import 'package:twinned_widgets/common/total_value_widget.dart';
 import 'package:twinned_widgets/common/value_distribution_pie_widget.dart';
@@ -20,6 +21,7 @@ final Map<String, TwinnedWidgetBuilder> _builders = {
   'TWValueDistributionPieChartWidget': ValueDistributionPieChartWidgetBuilder(),
   'TWTotalAndReportingAssetWidget': TotalAndReportingAssetWidgetBuilder(),
   'TWDeviceCartesianChartWidget': DeviceCartesianChartWidgetBuilder(),
+  'TWDeviceMultiFieldChartWidget': DeviceMultiFieldChartWidgetBuilder(),
 };
 
 class TwinnedWidgets {
