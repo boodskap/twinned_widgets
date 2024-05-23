cd01a2d119df1eee5bc512d2409d272916522f90
DhanuElavarasan1302 <nreldhanu1713@gmail.com>
2024-05-23 17:57:45 +0530

    Searchable multi select device dropdown implemented

--- lib/core/multi_device_dropdown.dart
+++ lib/core/multi_device_dropdown.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:nocode_commons/core/base_state.dart';
+import 'package:select2dot1/select2dot1.dart';
 import 'package:twinned_api/twinned_api.dart' as twin;
 import 'package:twinned_widgets/twinned_session.dart';
 
-typedef OnDevicesSelected = void Function(List<twin.Device> devices);
+typedef OnDevicesSelected = void Function(twin.Device? device);
 
 class MultiDeviceDropdown extends StatefulWidget {
-  final List<String> selectedDevices;
+  final String? selectedDevice;
   final OnDevicesSelected onDevicesSelected;
 
   const MultiDeviceDropdown({
     super.key,
-    required this.selectedDevices,
+    required this.selectedDevice,
     required this.onDevicesSelected,
   });
 
@@ -19,78 +19,82 @@ class MultiDeviceDropdown extends StatefulWidget {
   State<MultiDeviceDropdown> createState() => _MultiDeviceDropdownState();
 }
 
-class _MultiDeviceDropdownState extends BaseState<MultiDeviceDropdown> {
-  final List<DropdownMenuEntry<twin.Device>> _dropdownMenuEntries = [];
-  final List<twin.Device> _selectedDevices = [];
+class _MultiDeviceDropdownState extends State<MultiDeviceDropdown> {
+  twin.Device? _selectedDevice;
 
   @override
   Widget build(BuildContext context) {
-    if (_dropdownMenuEntries.isEmpty) {
-      return const Icon(Icons.hourglass_bottom);
-    }
-
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(width: 1, color: Colors.black38),
-        borderRadius: BorderRadius.circular(5),
-      ),
-      child: DropdownButtonFormField<twin.Device>(
-        items: _dropdownMenuEntries.map((entry) {
-          return DropdownMenuItem<twin.Device>(
-            value: entry.value,
-            child: CheckboxListTile(
-              title: Text(entry.label),
-              value: _selectedDevices.contains(entry.value),
-              onChanged: (isSelected) {
-                setState(() {
-                  if (isSelected == true) {
-                    _selectedDevices.add(entry.value!);
-                  } else {
-                    _selectedDevices.remove(entry.value);
-                  }
-                  widget.onDevicesSelected(_selectedDevices);
-                });
-              },
+    return FutureBuilder<List<SingleCategoryModel>>(
+      future: _search(''),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return Select2dot1(
+            selectDataController: SelectDataController(
+              data: snapshot.data ?? [],
             ),
+            pillboxContentMultiSettings:
+                const PillboxContentMultiSettings(pillboxOverload: 100),
+            onChanged: (selected) {
+              setState(() {
+                _selectedDevice = selected as twin.Device?;
+              });
+              widget.onDevicesSelected(_selectedDevice);
+            },
           );
-        }).toList(),
-        onChanged: (value) {},
-        isExpanded: true,
-      ),
+        }
+      },
     );
   }
 
-  Future load() async {
-    if (loading) return;
-    loading = true;
-    _dropdownMenuEntries.clear();
-    await execute(() async {
-      var pRes = await TwinnedSession.instance.twin.listDevices(
+  Future<List<SingleCategoryModel>> _search(String keyword) async {
+    List<SingleCategoryModel> items = [];
+
+    try {
+      var pRes = await TwinnedSession.instance.twin.searchDevices(
         apikey: TwinnedSession.instance.authToken,
-        body: const twin.ListReq(page: 0, size: 100),
+        body: twin.SearchReq(search: keyword, page: 0, size: 25),
       );
-      if (validateResponse(pRes)) {
-        List<String> names = [];
-        for (var dev in pRes.body!.values!) {
-          if (widget.selectedDevices.contains(dev.name)) {
-            _selectedDevices.add(dev);
-          }
-          if (names.contains(dev.name)) continue;
-          _dropdownMenuEntries.add(DropdownMenuEntry<twin.Device>(
-            label: dev.name,
-            value: dev,
+      if (pRes.body != null) {
+        for (var entity in pRes.body!.values!) {
+          items.add(SingleCategoryModel(
+            singleItemCategoryList: [
+              SingleItemCategoryModel(
+                  nameSingleItem:
+                      '${entity.name}, SN:${entity.deviceId} ${entity.description}')
+            ],
           ));
-          names.add(dev.name);
         }
       }
-    });
-
-    loading = false;
-    refresh();
+    } catch (e, s) {
+      debugPrint('$e\n$s');
+    }
+    return items;
   }
 
   @override
-  void setup() {
-    load();
+  void initState() {
+    super.initState();
+    _load();
+  }
+
+  Future<void> _load() async {
+    if (null == widget.selectedDevice || widget.selectedDevice!.isEmpty) return;
+    try {
+      var eRes = await TwinnedSession.instance.twin.getDevice(
+        apikey: TwinnedSession.instance.authToken,
+        deviceId: widget.selectedDevice!,
+      );
+      if (eRes != null && eRes.body != null) {
+        setState(() {
+          _selectedDevice = eRes.body!.entity;
+        });
+      }
+    } catch (e, s) {
+      debugPrint('$e\n$s');
+    }
   }
 }
