ee81aa9d02c82665edd963ab26e447696c1522ad
Thirumalvalavan <thirumalvalavan99@gmail.com>
2024-10-18 17:38:10 +0530

    Profile card widget added

--- lib/twinned_widgets.dart
+++ lib/twinned_widgets.dart
@@ -49,6 +49,7 @@ import 'package:twinned_widgets/common/multi_device_single_field_pie_chart_widge
 import 'package:twinned_widgets/common/multi_field_card_widget.dart';
 import 'package:twinned_widgets/common/multiple_device_model_chart_widget.dart';
 import 'package:twinned_widgets/common/multiple_line_min_max_average_widget.dart';
+import 'package:twinned_widgets/common/profile_card_widget.dart';
 import 'package:twinned_widgets/common/single_value_slider_widget.dart';
 import 'package:twinned_widgets/common/speedometer_widget.dart';
 import 'package:twinned_widgets/common/static_text_widget.dart';
@@ -161,6 +162,7 @@ final Map<String, TwinnedWidgetBuilder> _builders = {
   'TWGenericOddEvenEllipseWidget': GenericOddEvenEllipseWidgetBuilder(),
   'TWDeviceModelHeatmapWidget': DeviceModelHeatmapWidgetBuilder(),
   'TWSpeedometerWidget': SpeedometerWidgetBuilder(),
+  'TWProfileCardWidget': ProfileCardWidgetBuilder(),
 };
 
 class Tuple<K extends String, V extends TwinnedWidgetBuilder> {
