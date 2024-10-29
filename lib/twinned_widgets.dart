74a527314a147f9b8e7d0c6151d00048f6b42ea8
unknown <prithiviraina1998@gmail.com>
2024-10-29 14:46:04 +0530

    Generic Multi Shape Widget Implementation

--- lib/twinned_widgets.dart
+++ lib/twinned_widgets.dart
@@ -24,6 +24,7 @@ import 'package:twinned_widgets/common/field_card_widget.dart';
 import 'package:twinned_widgets/common/flow_meter_widget.dart';
 import 'package:twinned_widgets/common/generic_air_quality_circle_widget.dart';
 import 'package:twinned_widgets/common/generic_air_quality_linear_widget.dart';
+import 'package:twinned_widgets/common/generic_multi_shape_widget.dart';
 import 'package:twinned_widgets/common/generic_odd_even_circle_widget.dart';
 import 'package:twinned_widgets/common/generic_air_quality_widget.dart';
 import 'package:twinned_widgets/common/generic_day_weather_widget.dart';
@@ -165,6 +166,7 @@ final Map<String, TwinnedWidgetBuilder> _builders = {
   'TWSpeedometerWidget': SpeedometerWidgetBuilder(),
   'TWProfileCardWidget': ProfileCardWidgetBuilder(),
   'TWDeviceListCardWidget': DeviceListCardWidgetBuilder(),
+  'TWGenericMultiShapeWidget':GenericMultiShapeWidgetBuilder(),
 };
 
 class Tuple<K extends String, V extends TwinnedWidgetBuilder> {
